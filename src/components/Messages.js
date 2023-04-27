941a6d16ee11353f5e7dbd703b00956f32ea5d81
Vedran Stojnović <phidrho@gmail.com>
2023-04-27 19:11:52 +0200

    fix formatting

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,9 +1,9 @@
-import {Component} from "react";
+import { Component } from "react";
 import React from "react";
 
 class Messages extends Component {
   render() {
-    const {messages} = this.props;
+    const { messages } = this.props;
     return (
       <ul className="Messages-list">
         {messages.map(m => this.renderMessage(m))}
@@ -12,8 +12,8 @@ class Messages extends Component {
   }
 
   renderMessage(message) {
-    const {member, text} = message;
-    const {currentMember} = this.props;
+    const { member, text } = message;
+    const { currentMember } = this.props;
     const messageFromMe = member.id === currentMember.id;
     const className = messageFromMe ?
       "Messages-message currentMember" : "Messages-message";
@@ -21,7 +21,7 @@ class Messages extends Component {
       <li className={className}>
         <span
           className="avatar"
-          style={{backgroundColor: member.color}}
+          style={{ backgroundColor: member.color }}
         />
         <div className="Message-content">
           <div className="username">
