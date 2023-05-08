bfc0fceca4272992726733d67ea70cc8d783e0b1
Vedran Stojnović <phidrho@gmail.com>
2023-05-08 19:13:30 +0200

    sidebar pocetno stanje

---
+++ src/components/Sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './sidebar.css'
+
+class Sidebar extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false,
+        }
+
+        this.toggleSidebar = this.toggleSidebar.bind(this);
+    }
+
+    toggleSidebar(){
+        const {isOpen} = this.state;
+        this.setState({isOpen: !isOpen});
+    }
+
+
+
+    render(){
+        const {isOpen} = this.state;
+
+        return(
+        <div className="container-fluid mt-3">
+            <div className={`sidebar ${isOpen === true ? "active" : ""}`}>
+                <div className="sd-header">
+                    <h4>Sidebar Header</h4>
+                    <div className="btn btn-primary" onClick={this.toggleSidebar}>
+                        Close sidebar
+                    </div>
+                </div>
+                <div className="sd-body">
+                    <ul>
+                        <li><a href="#" className="sd-link">Menu item #1</a></li>
+                        <li><a href="#" className="sd-link">Menu item #2</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className={`sidebar-overlay ${isOpen === true ? "active" : ""}`} onClick={this.toggleSidebar}></div>
+        </div>
+        )
+    }
+}
+
+export default Sidebar;
