0b29d82f1a2ab961d216d02a7f348116bcf5a7d8
Vedran Stojnović <phidrho@gmail.com>
2023-05-11 19:10:37 +0200

    css fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Chat from './components/Chat';
 import Login from './components/Login';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
+import About from './components/About';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -19,19 +21,23 @@ class App extends React.Component {
     this.sidebar.toggleSidebar();
   }
 
+  handleLogout = () => {
+    this.setState({ username: "" });
+  }
+
   render() {
     return (
-      <div>
-        <Header toggleSidebar={this.toggleSidebar} username={this.state.username} />
+      <div className="App">
+        <Header toggleSidebar={this.toggleSidebar} username={this.state.username} handleLogout={this.handleLogout} />
         <Sidebar ref={(reference) => (this.sidebar = reference)} />
         <Routes>
           <Route path="/" element={this.state.username ? <Chat username={this.state.username} /> : <Navigate to="/login" />} />
           <Route path="/login" element={<Login onLogin={this.handleLogin} />} />
+          <Route path="/about" element={this.state.username ? <About /> : <Navigate to="/login" />} />
         </Routes>
       </div>
     );
   }
-
 }
 
 export default App;
