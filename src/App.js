6c454c7dbc0055144d6d09a92087e08f419b4602
Vedran Stojnović <phidrho@gmail.com>
2023-05-11 18:07:26 +0200

    dodali header, presložili iz chat js u app js i header, css fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Chat from './components/Chat';
 import Login from './components/Login';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,11 +15,21 @@ class App extends React.Component {
     this.setState({ username });
   }
 
+  toggleSidebar = () => {
+    this.sidebar.toggleSidebar();
+  }
+
   render() {
-    return <Routes>
-      <Route path="/" element={this.state.username ? <Chat username={this.state.username} /> : <Navigate to="/login" />} />
-      <Route path="/login" element={<Login onLogin={this.handleLogin} />} />
-    </Routes>
+    return (
+      <div>
+        <Header toggleSidebar={this.toggleSidebar} username={this.state.username} />
+        <Sidebar ref={(reference) => (this.sidebar = reference)} />
+        <Routes>
+          <Route path="/" element={this.state.username ? <Chat username={this.state.username} /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<Login onLogin={this.handleLogin} />} />
+        </Routes>
+      </div>
+    );
   }
 
 }
